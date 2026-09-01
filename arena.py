ba8122d76dcfe7a9c74d5675b10527ff55887116
Sakshi Rai <sakshi.rai.0308@gmail.com>
2026-09-01 23:34:37 +0530

    LLM Arena first commit

---
+++ arena.py
@@ -0,0 +1,28 @@
+import os, time
+from openai import OpenAI
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+groq_client   = OpenAI(api_key=os.getenv("GROQ_API_KEY"),
+                            base_url="https://api.groq.com/openai/v1")
+
+gemini_client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+def battle(prompt):
+    msgs = [{"role": "user", "content": prompt}]
+
+    try:
+        # Model A — Google Gemini
+        a = gemini_client.models.generate_content(model="gemini-flash-latest", contents=prompt)
+        gemini_reply = a.text
+    except Exception as e:
+        gemini_reply = f"Gemini busy(503), retrying... ({e})\n\nBut Groq Model answered!"
+
+    # Model B — Groq model
+    b = groq_client.chat.completions.create(model="openai/gpt-oss-20b", messages=msgs)
+    groq_reply = b.choices[0].message.content
+        
+
+    return gemini_reply, groq_reply 
